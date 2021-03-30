3c058c32d087fd7194b554afc492237f91d4db9b
Ahriane Ramos <76123138+misspx12@users.noreply.github.com>
2021-03-30 01:46:42 -0700

    Update Search.js
    replicate code from codesandbox to vs code - Search.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather Search Engine</h1>
+      <Search />
       <p></p>
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function Search() {
+export default function Search(props) {
+  const [city, setCity] = useState("");
+  const [temp, setTemp] = useState({});
+  const [submit, setSubmit] = useState(false);
 
+  function displayTemperature(response) {
+    setSubmit(true);
+    setTemp({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "d181817faaf7ac4148d91ac2cdf0c65a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" onChange={updateCity} placeholder="Enter a city" />
+      <button type="submit">Search</button>
+    </form>
+  );
+
+  if (submit) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(temp.temperature)}°C</li>
+          <li>Description: {temp.description}</li>
+          <li>Humidity: {temp.humidity}%</li>
+          <li>Wind: {temp.wind}km/h</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
